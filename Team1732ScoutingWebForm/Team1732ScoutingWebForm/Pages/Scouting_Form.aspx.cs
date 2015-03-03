f9a58b90b1d7419770bd76b3d18b62a523c9e866
Matthew Duffy <duffym22@gmail.com>
2015-03-02 23:38:00 -0600

    Lone commit

--- Team1732ScoutingWebForm/Team1732ScoutingWebForm/Pages/Scouting_Form.aspx.cs
+++ Team1732ScoutingWebForm/Team1732ScoutingWebForm/Pages/Scouting_Form.aspx.cs
@@ -7,16 +7,35 @@ using System.Web.UI.WebControls;
 
 namespace Team1732ScoutingWebForm
 {
-    enum bool Auto_Info
+    enum Stack_Locations
+    {
+        TT1 = 0,
+        TT2 = 1,
+        TT3 = 2,
+        TT4 = 3,
+        TT5 = 4,
+        Bin = 5,
+        Noodle = 6
+    }
+
+    struct Auto_Info
     {
         bool tote_set { get; set;}
-        
+        bool tote_stack { get; set; }
+        bool robot_set { get; set; }
+        bool mobility { get; set; }        
+    }
+
+    struct Coop_Info
+    {
+        bool stack_success { get; set; }
+        bool stack_attempt { get; set; }
+        bool set_success { get; set; }
+        bool set_attempt { get; set; }
     }
 
     public partial class _Default : System.Web.UI.Page
     {
-
-
         protected void enableMoarStacks_CheckedChanged(object sender, EventArgs e)
         {
             if (!toteStacksSixPlus.Visible)
